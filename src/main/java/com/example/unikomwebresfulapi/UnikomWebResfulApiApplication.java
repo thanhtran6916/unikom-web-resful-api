1340dc097ccb27269330d1449daf28211effa319
thanhtran <tranvanthanhtran.tt@gmail.com>
2021-12-30 13:48:03 +0700

    init

---
+++ src/main/java/com/example/unikomwebresfulapi/UnikomWebResfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.unikomwebresfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikomWebResfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnikomWebResfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unikomwebresfulapi/UnikomWebResfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unikomwebresfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnikomWebResfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
