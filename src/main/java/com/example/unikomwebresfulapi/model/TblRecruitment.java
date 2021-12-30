d38bdab9b86392d2fee78b674793d0eaea2549c2
thanhtran <tranvanthanhtran.tt@gmail.com>
2021-12-30 14:08:35 +0700

    create tbl_recruitment class

---
+++ src/main/java/com/example/unikomwebresfulapi/model/TblRecruitment.java
@@ -0,0 +1,50 @@
+package com.example.unikomwebresfulapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_recruitment")
+public class TblRecruitment extends Auditable<String> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "int", length = 11, nullable = false)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "responsibility", columnDefinition = "text")
+    private String responsibility;
+
+    @Column(name = "benefit", columnDefinition = "text")
+    private String benefit;
+
+    @Column(name = "experience",columnDefinition = "text")
+    private String experience;
+
+    @Column(name = "skill",columnDefinition = "text")
+    private String skill;
+
+    @Column(name = "attitude",columnDefinition = "text")
+    private String attitude;
+
+    @Column(name = "expireDate")
+    private LocalDate expireDate;
+
+    @Column(name = "salary")
+    private String salary;
+
+    @Column(name = "is_delete")
+    private boolean isDeleted;
+}
