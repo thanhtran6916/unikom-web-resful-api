34d81b288bfc030aa63370ee9d9ac42569238a34
Hunsume <you@example.com>
2021-12-30 15:13:33 +0700

    sua exception customer

--- src/main/java/com/example/unikomwebresfulapi/helper/CustomExceptionHandler.java
+++ src/main/java/com/example/unikomwebresfulapi/helper/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.unikomwebresfulapi.helper;
 
+import com.example.unikomwebresfulapi.exception.DuplicateRecordException;
 import com.example.unikomwebresfulapi.exception.ErrorResponse;
 import com.example.unikomwebresfulapi.exception.NotFoundException;
 import com.example.unikomwebresfulapi.exception.NotSaveException;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -21,5 +23,17 @@ public class CustomExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     public ErrorResponse handlerNotSaveException(NotSaveException e) {
         return new ErrorResponse(HttpStatus.NOT_IMPLEMENTED.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateRecordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDuplicateRecordException(DuplicateRecordException e, WebRequest req) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Yêu cầu bị từ chối");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerException(Exception e, WebRequest req) {
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Đã xảy ra lỗi! Vui lòng quay lại trang đăng nhập");
     }
 }
