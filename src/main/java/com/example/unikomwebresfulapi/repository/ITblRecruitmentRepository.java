c245e17adca2aa929396cb040fe3f5d0f93a36e9
thanhtran <tranvanthanhtran.tt@gmail.com>
2021-12-30 14:12:44 +0700

    create ITblRecruitmentRepository class

---
+++ src/main/java/com/example/unikomwebresfulapi/repository/ITblRecruitmentRepository.java
@@ -0,0 +1,16 @@
+package com.example.unikomwebresfulapi.repository;
+
+import com.example.unikomwebresfulapi.model.TblRecruitment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITblRecruitmentRepository extends JpaRepository<TblRecruitment, Long> {
+
+    @Query(value = "SELECT t FROM TblRecruitment t WHERE t.isDeleted = false ")
+    Page<TblRecruitment> findTblRecruitmentsExist(Pageable pageable);
+
+}
